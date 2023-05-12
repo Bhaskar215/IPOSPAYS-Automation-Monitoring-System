e2937cfec4df2d16022dfcbe9baacdab04f95ed0
Bhaskar <bhaskarmurthy215@gmail.com>
2023-05-12 23:29:57 +0530

    workspace specified

--- src/main/java/com/denovo/Pages/EmailSenderfile.java
+++ src/main/java/com/denovo/Pages/EmailSenderfile.java
@@ -8,26 +8,6 @@ public class EmailSenderfile {
 
     public static void main(String[] args) throws Exception {
 
-       /* String to = "recipient@example.com"; // recipient email address
-        String from = "sender@example.com"; // sender email address
-        String host = "smtp.example.com"; // SMTP server host
-
-        Properties properties = System.getProperties();
-        properties.setProperty("mail.smtp.host", host);
-
-        Session session = Session.getDefaultInstance(properties);
-
-        MimeMessage message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(from));
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-        message.setSubject("Subject of the email");
-
-        // Set the content of the message
-        message.setText("This is the message body");
-
-        // Send the message
-        Transport.send(message);
-        System.out.println("Message sent successfully.");*/
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
