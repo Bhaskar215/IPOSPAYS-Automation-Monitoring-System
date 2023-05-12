a7be1f160f9f95c2960807c75d60170b219af8b1
Bhaskar <bhaskarmurthy215@gmail.com>
2023-05-12 17:06:43 +0530

    email send with java now

--- src/test/java/com/denovo/testscripts/IsoLoginDashboardTest.java
+++ src/test/java/com/denovo/testscripts/IsoLoginDashboardTest.java
@@ -10,24 +10,25 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.util.Map;
 
-public class IsoLoginDashboardTest extends BaseTest{
+public class IsoLoginDashboardTest extends BaseTest {
 
-    LoginPage loginPage  = new LoginPage();
+    LoginPage loginPage = new LoginPage();
     DashboardPage dashboardPage = new DashboardPage();
 
 
     @Test(dataProvider = "getData")
-    public void validateIsoLoginDashboardIsLoaded(Map<String,String> data){
+    public void validateIsoLoginDashboardIsLoaded(Map<String, String> data) {
         loginPage.verifyLoginWithCorrectCredentials(data.get("ISOPrimaryEmail"), data.get("ISOPrimaryPWD"));
-          boolean isDashboardPage=dashboardPage.verifyDashboardIsLoaded();
-          //Assert.assertTrue(isDashboardPage);
+        boolean isDashboardPage = dashboardPage.verifyDashboardIsLoaded();
+        //Assert.assertTrue(isDashboardPage);
         Assert.fail();
     }
 
     @DataProvider
-    public  Object[][] getData() throws IOException {
-        Object[][] arrObject=null;
-        arrObject= DataProviderUtil.getExcelData(System.getProperty("user.dir")+"/src/main/java/com/denovo/ExcelData/SE_DataSheet.xlsx","IsoLoginDashboard");
+    public Object[][] getData() throws IOException {
+        Object[][] arrObject = null;
+        arrObject = DataProviderUtil.getExcelData(System.getProperty("user.dir") + "/src/main/java/com/denovo/ExcelData/SE_DataSheet.xlsx", "IsoLoginDashboard");
         return arrObject;
     }
+
 }
