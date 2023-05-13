2d25dabe22fba7efcf28a2b40cceb77b7279e2a7
Bhaskar <bhaskarmurthy215@gmail.com>
2023-05-13 13:47:21 +0530

    new Email utill added

--- src/main/java/com/denovo/Pages/EmailSenderfile.java
+++ src/main/java/com/denovo/Pages/EmailSenderfile.java
@@ -1,5 +1,7 @@
 package com.denovo.Pages;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
@@ -8,17 +10,16 @@ public class EmailSenderfile {
 
     public static void main(String[] args) throws Exception {
 
-        Properties props = new Properties();
+   /*     Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.transport.protocl","smpt");
+        props.put("mail.transport.protocol", "smpt");
 
 
         Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication()
-            {
+            protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication("bmurthy@denovosystem.com", "xqzjdicmbsdwumnp");
             }
         });
@@ -30,6 +31,49 @@ public class EmailSenderfile {
         message.setSubject("Build failed");
         message.setText("The build failed. Please check the logs.");
 
+        Transport.send(message);*/
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.transport.protocol", "smpt");
+
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("bmurthy@denovosystem.com","xqzjdicmbsdwumnp");
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        message.setSubject("IPOSPAYS MONITOR SYSTEM REPORT");
+        message.setContent("<h1>IPOSPAYS FAILED REPORT<h1>","text/hmtl");
+
+        Address addressTo = new InternetAddress("bhaskarmurthy215@gmail.com");
+
+        // Address address2To = new InternetAddress("bhaskarmurthy@gmail.com");
+
+        message.setRecipient(Message.RecipientType.TO,addressTo);
+        //To send Multiple people
+        // message.addRecipient(Message.RecipientType.TO,address2To);
+
+        MimeMultipart multipart = new MimeMultipart();
+        MimeBodyPart attachment = new MimeBodyPart();
+        attachment.attachFile(new File("extent-test-output/ExtentReport.html"));
+
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent("<h1>IPOSPAYS FAILED REPORT<h1>","text/hmtl");
+        multipart.addBodyPart(messageBodyPart);
+        multipart.addBodyPart(attachment);
+
+        message.setContent(multipart);
+
         Transport.send(message);
+
+
     }
-}
+
+    }
--- src/test/java/com/denovo/testscripts/SendEmailTest.java
+++ src/test/java/com/denovo/testscripts/SendEmailTest.java
@@ -3,16 +3,16 @@ package com.denovo.testscripts;
 import org.testng.annotations.Test;
 
 import javax.mail.*;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.mail.internet.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
 public class SendEmailTest {
 
     @Test
-    public void sendEmailTest() throws MessagingException {
-        Properties props = new Properties();
+    public void sendEmailTest() throws MessagingException, IOException {
+        /*Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
         props.put("mail.smtp.auth", "true");
@@ -33,7 +33,48 @@ public class SendEmailTest {
         message.setSubject("Build failed");
         message.setText("The build failed. Please check the logs.");
 
+        Transport.send(message);*/
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.transport.protocol", "smpt");
+
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("bmurthy@denovosystem.com","xqzjdicmbsdwumnp");
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        message.setSubject("IPOSPAYS MONITOR SYSTEM REPORT");
+        message.setContent("<h1>IPOSPAYS FAILED REPORT<h1>","text/hmtl");
+
+        Address addressTo = new InternetAddress("bhaskarmurthy215@gmail.com");
+
+        // Address address2To = new InternetAddress("bhaskarmurthy@gmail.com");
+
+        message.setRecipient(Message.RecipientType.TO,addressTo);
+        //To send Multiple people
+        // message.addRecipient(Message.RecipientType.TO,address2To);
+
+        MimeMultipart multipart = new MimeMultipart();
+        MimeBodyPart attachment = new MimeBodyPart();
+        attachment.attachFile(new File("extent-test-output/ExtentReport.html"));
+
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent("<h1>IPOSPAYS FAILED REPORT<h1>","text/hmtl");
+        multipart.addBodyPart(messageBodyPart);
+        multipart.addBodyPart(attachment);
+
+        message.setContent(multipart);
+
         Transport.send(message);
+
     }
-    }
+}
 
