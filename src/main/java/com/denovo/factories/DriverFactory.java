6192fb772a22d0b588eb72d5a6ff11f99c380eb6
Bhaskar <bhaskarmurthy215@gmail.com>
2023-05-15 16:01:11 +0530

    new headless added

--- src/main/java/com/denovo/factories/DriverFactory.java
+++ src/main/java/com/denovo/factories/DriverFactory.java
@@ -14,6 +14,8 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public final class DriverFactory {
 
@@ -31,11 +33,26 @@ public final class DriverFactory {
             if(runmode.equalsIgnoreCase("remote")) {
                 DesiredCapabilities cap = new DesiredCapabilities();
                 ChromeOptions options = new ChromeOptions();
+               /* options.addArguments("--headless");
                 options.addArguments("no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
                 cap.setBrowserName(BrowserType.CHROME);
                 driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+                driver.manage().window().fullscreen();*/
+
+                options.setCapability("selenoid:options", new HashMap() {{
+                    put("name", "Test badge...");
+                    put("sessionTimeout", "5m");
+                    put("screenResolution","1920x1080x24");
+                    put("enableVNC", true); // if true While exection test case we will see video
+                    put("enableVideo", false); //if true while execution it will save the video
+                    put("env", new ArrayList<String>() {{add("TZ=UTC"); }});
+                    put("labels", new HashMap<String, Object>()
+                    {{put("manual", "true");}});
+                }});
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
                 driver.manage().window().fullscreen();
+
             }
             else{
                 WebDriverManager.chromedriver().setup();
