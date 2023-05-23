0d390067c7eeee5e7300cc6a42fcfb771012f969
Bhaskar <bhaskarmurthy215@gmail.com>
2023-05-23 18:49:51 +0530

    ip address changes for safari too

--- src/main/java/com/denovo/factories/DriverFactory.java
+++ src/main/java/com/denovo/factories/DriverFactory.java
@@ -80,7 +80,7 @@ public final class DriverFactory {
                     put("labels", new HashMap<String, Object>()
                     {{put("manual", "true");}});
                 }});
-                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+                driver = new RemoteWebDriver(new URL("http://selenoid:4444/wd/hub"), options);
 
             }
             else {
